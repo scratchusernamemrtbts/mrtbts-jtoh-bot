1bdb7e78b6bb2f46f6fec743407b0ed23fbe1af1
scratchusernamemrtbts <66320961+scratchusernamemrtbts@users.noreply.github.com>
2024-09-19 11:55:48 +0700

    talk command

--- index.js
+++ index.js
@@ -44,11 +44,15 @@ client.on(Events.InteractionCreate, async interaction => {
 	try {
 		await command.execute(interaction)
 	} catch (error) {
-		log(error, 'error')
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true })
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+		try {
+			log(error, 'error')
+			if (interaction.replied || interaction.deferred) {
+				await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true })
+			} else {
+				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+			}
+		} catch {
+			console.error(error)
 		}
 	} 
 })
